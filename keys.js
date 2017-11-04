8373cc37f6b4be045b897527ec71aa7e2d92133a
David <djlowes@gmail.com>
2017-11-04 16:37:53 -0700

    updates

--- keys.js
+++ keys.js
@@ -1,10 +1,18 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 var twitterKeys = {
   consumer_key: 'LKZ1mRv94YzKIdkI2Ss7C12gJ',
   consumer_secret: 'bH1QRcmBwNJjo8akpyvostnxls0uHyiEaLLXWbWoAbDYzXmlFo',
   access_token_key: '214569195-mWSJf9NfA37as7qC2D824oBBex0idQgzvI0L5Uq9',
-  access_token_secret: 'PWxsy26wE909JGpQzvhAqmXbrQP2G9eRVmMjd90ZEyDHH',
+  access_token_secret: 'PWxsy26wE909JGpQzvhAqmXbrQP2G9eRVmMjd90ZEyDHH'
 }
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+  clientID: 'cc8b067f81a24ee2b49e96cecf496480',
+  clientSecret: '30bca62440d74eb2b1de293437238b32'
+}
+
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys
+}
--- liri.js
+++ liri.js
@@ -1,27 +1,76 @@
 var keys = require("./keys.js");
 var fs = require("fs");
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
 
-console.log("--------------------------");
-console.log("Twitter Keys and Access Tokens");
-console.log(keys);
-console.log("--------------------------");
+
+// console.log("--------------------------");
+// console.log("Twitter Keys and Access Tokens");
+// console.log(keys);
+// console.log("--------------------------");
 
 var command = process.argv[2];
+var movieName = "";
 
-switch (command) {
-  case "my-tweets":
-    tweets();
-    break;
+//Determine which function to run
+main();
+function main() {
+  switch (command) {
+    case "my-tweets":
+      tweets();
+      break;
 
-  case "spotify-this-song":
-    spotify();
-    break;
+    case "spotify-this-song":
+      spotify();
+      break;
 
-  case "movie-this":
-    imdb();
-    break;
+    case "movie-this":
+      imdb();
+      break;
 
-  case "do-what-it-says":
-    simon();
-    break;
+    case "do-what-it-says":
+      simon();
+      break;
+  }
+}
+
+//my-tweets
+function tweets() {
+  let client = new twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+  });
+
+  let params = {
+    screen_name: 'djlowes',
+    count: 20
+  };
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error && response.statusCode === 200) {
+      for (let i=0; i<tweets.length; i++) {
+        console.log(tweets[i].user.screen_name + ": " + tweets[i].text);
+      }
+    }
+    else {
+      console.log("Tweets failed to load, this is ther error: " + error)
+    }
+  });
+}
+
+// spotify-this-song
+let client = new spotify({
+    id: keys.spotifyKeys.clientID,
+    secret: keys.spotifyKeys.clientSecret
+});
+
+  spotify.search({ type: 'track', query: command }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+  console.log(data);
+  });
 }
