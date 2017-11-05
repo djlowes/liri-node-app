12567629a6b40a92de3ce466edce3dfcb2a5b133
David <djlowes@gmail.com>
2017-11-04 21:12:44 -0700

    updates2

--- keys.js
+++ keys.js
@@ -1,18 +1,16 @@
 // console.log('this is loaded');
 
-var twitterKeys = {
-  consumer_key: 'LKZ1mRv94YzKIdkI2Ss7C12gJ',
-  consumer_secret: 'bH1QRcmBwNJjo8akpyvostnxls0uHyiEaLLXWbWoAbDYzXmlFo',
-  access_token_key: '214569195-mWSJf9NfA37as7qC2D824oBBex0idQgzvI0L5Uq9',
-  access_token_secret: 'PWxsy26wE909JGpQzvhAqmXbrQP2G9eRVmMjd90ZEyDHH'
+var keys = {
+  twitterKeys: {
+    consumer_key: 'LKZ1mRv94YzKIdkI2Ss7C12gJ',
+    consumer_secret: 'bH1QRcmBwNJjo8akpyvostnxls0uHyiEaLLXWbWoAbDYzXmlFo',
+    access_token_key: '214569195-mWSJf9NfA37as7qC2D824oBBex0idQgzvI0L5Uq9',
+    access_token_secret: 'PWxsy26wE909JGpQzvhAqmXbrQP2G9eRVmMjd90ZEyDHH'
+  },
+  spotifyKeys: {
+    client_ID: 'cc8b067f81a24ee2b49e96cecf496480',
+    client_Secret: 'e133dca456fc4b0c86be3612454c2357'
+  },
 }
 
-var spotifyKeys = {
-  clientID: 'cc8b067f81a24ee2b49e96cecf496480',
-  clientSecret: '30bca62440d74eb2b1de293437238b32'
-}
-
-module.exports = {
-  twitterKeys: twitterKeys,
-  spotifyKeys: spotifyKeys
-}
+module.exports = keys;
--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
+//Dependencies
 var keys = require("./keys.js");
 var fs = require("fs");
 var twitter = require('twitter');
 var spotify = require('node-spotify-api');
+var request = require('request');
 
 
 // console.log("--------------------------");
@@ -9,11 +11,16 @@ var spotify = require('node-spotify-api');
 // console.log(keys);
 // console.log("--------------------------");
 
+//Select from 4 command options
 var command = process.argv[2];
-var movieName = "";
+
+//Allows more than one word of text for command inputs
+var query = process.argv.slice(3).join(" ");
+
 
 //Determine which function to run
 main();
+
 function main() {
   switch (command) {
     case "my-tweets":
@@ -42,35 +49,63 @@ function tweets() {
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret
   });
-
   let params = {
     screen_name: 'djlowes',
     count: 20
   };
-
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error && response.statusCode === 200) {
-      for (let i=0; i<tweets.length; i++) {
+      for (let i = 0; i < tweets.length; i++) {
         console.log(tweets[i].user.screen_name + ": " + tweets[i].text);
       }
-    }
-    else {
-      console.log("Tweets failed to load, this is ther error: " + error)
+    } else {
+      console.log("Tweets failed to load, this is the error: " + error)
     }
   });
 }
 
 // spotify-this-song
-let client = new spotify({
-    id: keys.spotifyKeys.clientID,
-    secret: keys.spotifyKeys.clientSecret
-});
-
-  spotify.search({ type: 'track', query: command }, function(err, data) {
+function spotify() {
+  let spot = new spotify({
+    id: keys.spotifyKeys.client_ID,
+    secret: keys.spotifyKeys.client_Secret
+  });
+  if (!query) {
+    query = "The Sign"
+  }
+  let params = {
+    type: 'track',
+    query: query
+  };
+  spot.search(params, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-
-  console.log(data);
+    console.log(data);
   });
+}
+
+// movie-this
+function imdb() {
+  if (!query) {
+    query = 'Mr. Nobody.'
+  }
+  var queryURL = "https://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=40e9cece";
+  request.get(queryURL, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      // console.log(JSON.parse(body))
+      console.log("Movie Title: " + JSON.parse(body).Title)
+      console.log("Year Released: " + JSON.parse(body).Year)
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating)
+      console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value)
+      console.log("Country: " + JSON.parse(body).Country)
+      console.log("Language: " + JSON.parse(body).Language)
+      console.log("Plot: " + JSON.parse(body).Plot)
+      console.log("Actors: " + JSON.parse(body).Actors)
+      }
+    else {
+      console.log("Movie details failed to load, this is the error: " + error)
+    }
+  })
+
 }
